75801d67324f8f7e6a52014946705812113fa4a8
Face70 <60823541+DarkKnight70@users.noreply.github.com>
2020-05-29 23:44:50 -0600

    added to the connection file and the seedssql

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name) VALUES ("Double Double");
+INSERT INTO burgers (burger_name) VALUES ("Celestial Burger");
+INSERT INTO burgers (burger_name) VALUES ("Famous Star");
+INSERT INTO burgers (burger_name) VALUES ("Dave's Classic Double");
+INSERT INTO burgers (burger_name) VALUES ("Jucy Lucy");
+INSERT INTO burgers (burger_name) VALUES ("Shack Burger");
